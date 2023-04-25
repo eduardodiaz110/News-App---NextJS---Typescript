8eec68ed8ca8a89cec32d4a652a6657d8eb724bd
eduardodiaz110 <112425228+eduardodiaz110@users.noreply.github.com>
2023-04-25 15:56:43 -0600

    5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { SSRProvider } from '@react-aria/ssr';
 import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Container } from 'react-bootstrap';
-import styles from "@/styles/App.module.css"
+import styles from "@/styles/App.module.css";
 import NavBar from '@/components/NavBar';
 import NextNProgress from "nextjs-progressbar";
 
-
 const inter = Inter({ subsets: ['latin'] });
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SSRProvider>
-
-
     <div className={inter.className}>
-
-        <Head>
-        <title key="title">News App</title>
-        <meta name="description" key="description" content="Next JS News App" />
+      <Head>
+        <title key="title">NextJS News App</title>
+        <meta name="description" key="description" content="NextJS crash course by Coding in Flow" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NextNProgress/>
-      <NavBar/>
+      <NextNProgress />
+      <NavBar />
       <Container className={styles.pageContainer}>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
       </Container>
     </div>
-
-    </SSRProvider>
-
-  )
+  );
 }
--- pages/categories/[category].tsx
+++ pages/categories/[category].tsx
@@ -3,70 +3,63 @@ import { NewsArticle, NewsResponse } from "@/models/NewsArticles";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import {Alert} from "react-bootstrap";
+import { Alert } from "react-bootstrap";
 
-interface CategoryNewsPageProps{
+interface CategoryNewsPageProps {
     newsArticles: NewsArticle[],
 }
 
-export const getStaticPaths: GetStaticPaths = async () =>{
-    const categorySlugs = [
+export const getStaticPaths: GetStaticPaths = async () => {
+    const categorySlugs = [ // this could be coming from an API
         "business",
         "entertainment",
         "general",
         "health",
         "science",
         "sports",
-        "technology"
+        "technology",
     ];
 
-    const paths = categorySlugs.map(slug=> ({ params:{ category: slug}}));
+    const paths = categorySlugs.map(slug => ({ params: { category: slug } }));
 
     return {
         paths,
-        fallback: true,
+        fallback: false,
     }
 }
 
-
-export const getStaticProps: GetStaticProps<CategoryNewsPageProps> = async({params}) =>{
+export const getStaticProps: GetStaticProps<CategoryNewsPageProps> = async ({ params }) => {
     const category = params?.category?.toString();
-    const response = await fetch (`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.NEWS_API_KEY}`)
+    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.NEWS_API_KEY}`);
     const newsResponse: NewsResponse = await response.json();
-    console.log(newsResponse.articles); 
-
-  return {
-
-    props: {newsArticles: newsResponse.articles},
-    revalidate: 5*60,
-
-  }
+    return {
+        props: { newsArticles: newsResponse.articles },
+        revalidate: 5 * 60,
+    }
+    // let error go to 500 page
 }
 
-const CategoryNewsPage = ({newsArticles}: CategoryNewsPageProps) => {
-
+const CategoryNewsPage = ({ newsArticles }: CategoryNewsPageProps) => {
     const router = useRouter();
     const categoryName = router.query.category?.toString();
 
-    if (router.isFallback) {
-        return <div>404...</div>
-      }
-    
+    const title = "Category: " + categoryName;
 
-    const title = "Category: " +categoryName;
-    return ( 
+    return (
         <>
-        <Head>
-            <title key="title">{`${title} - NextJS News App`}</title>
-        </Head>
-        <main>
-            <h1>{title}</h1>
-            <Alert className="mb-5"> This page uses <strong>getStaticProps</strong> for very high page loading and <strong>incremental static regeneration</strong> to show data not older than <strong>5 minutes</strong>.</Alert>
-            <NewsArticlesGrid articles={newsArticles}/>
-        </main>
-
+            <Head>
+                <title key="title">{`${title} - NextJS News App`}</title>
+            </Head>
+            <main>
+                <h1>{title}</h1>
+                <Alert>
+                    This is page uses <strong>getStaticProps</strong> for very high page loading speed
+                    and <strong>incremental static regeneration</strong> to show data not older than <strong>5 minutes</strong>.
+                </Alert>
+                <NewsArticlesGrid articles={newsArticles} />
+            </main>
         </>
-     );
+    );
 }
- 
+
 export default CategoryNewsPage;
